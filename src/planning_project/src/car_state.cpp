cdf727395d67c64831ed16253c90f7f37c701cca
root <root@LAPTOP-CHI>
2023-12-06 00:54:15 -0500

    fuck this

--- src/planning_project/src/car_state.cpp
+++ src/planning_project/src/car_state.cpp
@@ -2,22 +2,29 @@
 #include "car_state.h"
 
 // next car state kinematic update
-CarState CalcNextState(CarState state, double accel, double delta_alpha)
-{
+CarState CalcNextState(CarState state, double accel, double delta_alpha) {
+
     // next car state
     CarState next;
 
     // update time
     next.t = state.t + dt;
+
+    // TODO(chig): update x
     next.x = state.x + dt * cos(state.yaw + delta_alpha) * state.vel;
+
+    // TODO(chig): update y
     next.y = state.y + dt * sin(state.yaw + delta_alpha) * state.vel;
+    
+    // TODO(chig): update yaw
     next.yaw = state.yaw + dt * state.vel * tan(state.alpha) / wheel_base;
-    if (next.yaw < 0) {
-        next.yaw += 2 * pi;
-    }
+
     // TODO(chig): update vel
     next.vel = state.vel + dt * accel;
+    
+    // TODO(chig): update steering
     next.alpha = state.alpha + dt * delta_alpha;
 
     return next;
+
 }
